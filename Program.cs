dc5255e3e18ef8d968d3793f11a675e6650002d8
RezaIndraMaulana <rezaindramaulana9@gmail.com>
2025-03-10 02:57:48 +0700

    menambahkan implementasi dengan table-driven ( yang benar )

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace tpmodul4_103022300109
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Daftar Kelurahan");
+            Console.WriteLine("Kelurahan\t |    Kode Pos");
+            Console.WriteLine("-------------------------------");
+            foreach (KodePos.kelurahan kelurahan in Enum.GetValues(typeof(KodePos.kelurahan)))
+            {
+                String namaKelurahan = kelurahan.ToString();
+                Console.WriteLine($"{kelurahan}\t | {KodePos.getKodePos(kelurahan)}");
+            }
         }
     }
 }
