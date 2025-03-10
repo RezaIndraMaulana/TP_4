b671d53f10d069a818a8efa596f11a0093d99274
RezaIndraMaulana <rezaindramaulana9@gmail.com>
2025-03-10 21:37:09 +0700

    menambahkan implementasi dengan state-based construction

---
+++ DoorMachine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_103022300109
+{
+    public enum pintu { terkunci, terbuka };
+    public enum kegiatan { bukaPintu, kunciPintu };
+    class DoorMachine
+    {
+        private pintu state;
+        public DoorMachine()
+        {
+            state = pintu.terkunci;
+        }
+
+        public void ubahPintu(kegiatan option) {
+            switch (state) {
+                case pintu.terkunci:
+                    if (option == kegiatan.bukaPintu)
+                    {
+                        Console.WriteLine("Pintu tidak terkunci");
+                        state = pintu.terbuka;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Pintu sudah terkunci");
+                    }
+                    break;
+                case pintu.terbuka:
+                    if (option == kegiatan.kunciPintu)
+                    {
+                        Console.WriteLine("Pintu terkunci");
+                        state = pintu.terkunci;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Pintu sudah terbuka");
+                    }
+                    break;
+            }
+        }
+        public void getStatusPintu()
+        {
+            Console.WriteLine("Status Pintu: " + state);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,6 +14,27 @@ namespace tpmodul4_103022300109
                 String namaKelurahan = kelurahan.ToString();
                 Console.WriteLine($"{kelurahan}\t | {KodePos.getKodePos(kelurahan)}");
             }
+
+            Console.WriteLine("-------------------------------");
+
+            DoorMachine doorMachine = new DoorMachine();
+
+            doorMachine.getStatusPintu();   
+            String input = "";
+            while (input != "keluar")
+            {
+                Console.WriteLine("Perintah (bukaPintu / kunciPintu)");
+                input = Console.ReadLine();
+                if(Enum.TryParse(input, out kegiatan kegiatan))
+                {
+                    doorMachine.ubahPintu(kegiatan);
+                }
+                else
+                {
+                    Console.WriteLine("Perintah tidak valid");
+                }
+            }
+            Console.WriteLine("-----------------------------");
         }
     }
 }
